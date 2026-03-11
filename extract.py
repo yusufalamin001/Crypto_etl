42fe30ceec9e8215aa449d78b4fe127ce220be66
Al-amin <yusufalamin027@gmail.com>
2026-03-12 00:11:59 +0100

    Add retry logic to extract.py

--- extract.py
+++ extract.py
@@ -1,13 +1,17 @@
 import requests
 from config import API_URL
 import logging
+import time
 
 def extract():
-    try:
-        response=requests.get(API_URL)
-        response.raise_for_status()
-        raw_data=response.json()
-        return raw_data
-    except Exception as e:
-        logging.error(f"Error extracting data: {e}")
-        return []
+    for attempt in range(3):
+        try:
+            response=requests.get(API_URL)
+            response.raise_for_status()
+            raw_data=response.json()
+            return raw_data
+        except Exception as e:
+            logging.warning(f"Attempt {attempt + 1} failed, retrying...: {e}")
+            time.sleep(10)
+    logging.error("Extraction failed")
+    return []
